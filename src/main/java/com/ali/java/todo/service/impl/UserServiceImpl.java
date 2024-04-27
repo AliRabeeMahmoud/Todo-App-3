166c75a51efca4d4accbac485caf0751673320ff
AliRabeeMahmoud <147240981+AliRabeeMahmoud@users.noreply.github.com>
2024-04-27 13:12:36 +0200

    Update UserServiceImpl.java

--- src/main/java/com/ali/java/todo/service/impl/UserServiceImpl.java
+++ src/main/java/com/ali/java/todo/service/impl/UserServiceImpl.java
@@ -33,6 +33,7 @@ public class UserServiceImpl implements UserService {
         oldUser.setPassword(user.getPassword());
         oldUser.setFirstName(user.getFirstName());
         oldUser.setLastName(user.getLastName());
+        userRepository.save(oldUser);
         return oldUser;
     }
 
